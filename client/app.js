4a9fbacd7887633d4bce2683008fc5e2bd0faf4f
Kamil Put <kamil.put@interia.pl>
2021-09-09 08:16:28 +0200

    Init project, add html and styles

---
+++ client/app.js
@@ -0,0 +1,8 @@
+const loginForm = '#welcome-form';
+const messagesSection = '#messages-section';
+const messagesList = '#messages-list';
+const addMessageForm = '#add-messages-form';
+const userNameInput = '#username';
+const messageContentInput = '#message-content';
+
+let userName;
